0478773c38040b073ff84a5062f58fd5cb465e47
Vishalj154 <vishaljankar2534@gmail.com>
2026-06-20 16:21:55 +0530

    login part started , signinwithemailandpassword imported and added handlesubmit

--- frontend/react/src/login.jsx
+++ frontend/react/src/login.jsx
@@ -1,21 +1,34 @@
 import React from 'react'
 import { useState } from 'react'
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "./firebase";
 import './index.css'
 
 const Login = () => {
-    const [username, setUsername] = useState('')
+    const [Email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+
+    const handleSubmit = (e)=>{
+        e.preventDefault();
+        const userinfo = new signInWithEmailAndPassword(
+            auth,
+            Email,
+            password
+        );
+        console.log(userinfo.user);
+        alert("Login successful")
+    }
     
   return (
     <div className='signup-form'>
         <h1>Login here</h1>
         <label >
-            Username: <input />
+            Email: <input type="email" value={Email} onChange={(e)=> setEmail(e.target.value)  } required/>
         </label>
         <label >
-            Password: <input />
+            Password: <input type="password" value={password} onChange={(e)=> setPassword(e.target.value)  } required/>
         </label>
-        <button className='submit' type='submit' >submit</button>
+            <button className="submit" onClick={handleSubmit} type="button">Submit</button>
     </div>
     
   )
